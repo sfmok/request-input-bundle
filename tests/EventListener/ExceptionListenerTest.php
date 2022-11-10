a112a45b33c098fb6827da62091196ada6846b58
Mokhtar Tlili <mokhtar.tlili@eqs.com>
2022-11-10 17:42:44 +0100

    enhance phpunit tests

--- tests/EventListener/ExceptionListenerTest.php
+++ tests/EventListener/ExceptionListenerTest.php
@@ -6,6 +6,7 @@ namespace Sfmok\RequestInput\Tests\EventListener;
 
 use PHPUnit\Framework\TestCase;
 use Prophecy\PhpUnit\ProphecyTrait;
+use Prophecy\Prophecy\ObjectProphecy;
 use Sfmok\RequestInput\EventListener\ExceptionListener;
 use Sfmok\RequestInput\Exception\ValidationException;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,17 +20,25 @@ class ExceptionListenerTest extends TestCase
 {
     use ProphecyTrait;
 
-    public function testOnKernelException(): void
+    private ObjectProphecy $serializer;
+    private ObjectProphecy $httpKernel;
+
+    protected function setUp(): void
+    {
+        $this->serializer = $this->prophesize(SerializerInterface::class);
+        $this->httpKernel = $this->prophesize(HttpKernelInterface::class);
+    }
+
+    public function testOnKernelExceptionWithValidationException(): void
     {
         $serializedConstraintViolationList = '{"foo": "bar"}';
         $list = new ConstraintViolationList([]);
 
-        $serializerProphecy = $this->prophesize(SerializerInterface::class);
-        $serializerProphecy->serialize($list, 'json')->willReturn($serializedConstraintViolationList)->shouldBeCalled();
+        $this->serializer->serialize($list, 'json')->willReturn($serializedConstraintViolationList)->shouldBeCalled();
 
-        $listener = new ExceptionListener($serializerProphecy->reveal());
+        $listener = new ExceptionListener($this->serializer->reveal());
         $event = new ExceptionEvent(
-            $this->prophesize(HttpKernelInterface::class)->reveal(),
+            $this->httpKernel->reveal(),
             new Request(),
             HttpKernelInterface::MAIN_REQUEST,
             new ValidationException($list)
@@ -42,5 +51,20 @@ class ExceptionListenerTest extends TestCase
         $this->assertSame($serializedConstraintViolationList, $response->getContent());
         $this->assertSame(Response::HTTP_BAD_REQUEST, $response->getStatusCode());
         $this->assertSame('application/problem+json; charset=utf-8', $response->headers->get('Content-Type'));
+    }
+
+    public function testOnKernelExceptionWithoutValidationException(): void
+    {
+        $this->serializer->serialize()->shouldNotBeCalled();
+
+        $listener = new ExceptionListener($this->serializer->reveal());
+        $event = new ExceptionEvent(
+            $this->httpKernel->reveal(),
+            new Request(),
+            HttpKernelInterface::MAIN_REQUEST,
+            new \Exception()
+        );
+
+        $listener->onKernelException($event);
     }
 }
